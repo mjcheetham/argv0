3daef0fd0b70684b6f3aa551c3c53de9f888c821
Matthew John Cheetham <mjcheetham@outlook.com>
2022-11-03 17:21:11 -0700

    Add support for net461 and fix macos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.IO;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 
-string? argv0 = null;
+string argv0 = null;
 
+#if NETFRAMEWORK
+argv0 = GetWindowsArgv0();
+#else
 if (OperatingSystem.IsWindows())
 {
 	argv0 = GetWindowsArgv0();
@@ -15,35 +21,42 @@ else if (OperatingSystem.IsLinux())
 {
 	argv0 = GetLinuxArgv0();
 }
-
-argv0 ??= Process.GetCurrentProcess().MainModule?.FileName
-	?? Environment.GetCommandLineArgs()[0];
+#endif
 
 Console.WriteLine(argv0);
 
-static string? GetLinuxArgv0()
+static string GetLinuxArgv0()
 {
 	string cmdline = File.ReadAllText("/proc/self/cmdline");
 	return cmdline.Split('\0')[0];
 }
 
-static string? GetMacOSArgv0()
+const int MAXPATHLEN = 1024;
+const int PROC_PIDPATHINFO_MAXSIZE = 4 * MAXPATHLEN;
+
+static unsafe string GetMacOSArgv0()
 {
-	IntPtr ptr = _NSGetArgv();
-	IntPtr argvPtr = Marshal.ReadIntPtr(ptr);
-	IntPtr argv0Ptr = Marshal.ReadIntPtr(argvPtr);
-	return Marshal.PtrToStringAnsi(argv0Ptr);
+	var pid = Process.GetCurrentProcess().Id;
+	int result = 0;
+	byte* pBuffer = stackalloc byte[PROC_PIDPATHINFO_MAXSIZE];
+	result = proc_pidpath(pid, pBuffer, (uint)(PROC_PIDPATHINFO_MAXSIZE * sizeof(byte)));
+	if (result <= 0)
+	{
+		return null;
+	}
+
+	return Encoding.UTF8.GetString(pBuffer, result);
 }
 
-static string? GetWindowsArgv0()
+static string GetWindowsArgv0()
 {
 	IntPtr argvPtr = CommandLineToArgvW(GetCommandLine(), out _);
 	IntPtr argv0Ptr = Marshal.ReadIntPtr(argvPtr);
 	return Marshal.PtrToStringAuto(argv0Ptr);
 }
 
-[DllImport("libc", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-static extern IntPtr _NSGetArgv();
+[DllImport("/usr/lib/libproc.dylib", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+static extern unsafe int proc_pidpath(int pid, byte* buffer, uint bufferSize);
 
 [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
 static extern IntPtr GetCommandLine();
